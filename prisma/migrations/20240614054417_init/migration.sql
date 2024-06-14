a62fad29a648122a6cd487ca9e894ae359e7068f
Alvin Setiawan <alvin.set25@gmail.com>
2024-06-14 12:44:42 +0700

    refactor : prisma migration

--- prisma/migrations/20240614054417_init/migration.sql
+++ prisma/migrations/20240614054417_init/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `Users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `username` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `Users_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
